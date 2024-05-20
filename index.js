3658d8c942c6c5e301c6320aff693a51bfa6bd2b
zamzamnn <zamzamnoorahmed73@gmail.com>
2024-05-20 17:39:34 +0300

    phase_1_control_flow_lab

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(distance){
+  
+    if (distance <= 400){
+      console.log('this on me')
+    }else if (distance > 2500){
+      console.log("I will gladly take your thirty bucks")
+    }else {
+      return "No can do.";
+    }
+  }
+  function scuberGreetingForFeet(distance) {
+    if (distance <= 400) {
+      return "This one is on me!";
+    } else if (distance > 400 && distance <= 2500) {
+      return "I will gladly take your thirty bucks.";
+    } else {
+      return "No can do.";
+    }
+  }
+  
+  (Optional) 
+  let greeting = scuberGreetingForFeet(300); 
+  console.log(greeting);
+  
+  
+  function ternaryCheckCity(city){
+    return city === 'NYC' ? "Ok, sounds good." : "No go." ;
+  }
+  
+  
+    
+    function switchOnCharmFromTip(tip) {
+      switch (tip) {
+        case 'generous':
+         return 'Thank you so much.';
+          break;
+  case 'not as generous':
+    return 'Thank you.'
+        default:
+          return "Bye.";
+      }
+    }
